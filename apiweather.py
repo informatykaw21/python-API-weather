41ddc75ce1e745c99b8fd38f3be7f989b8968d39
informatykaw21 <informatyk.aw21@gmail.com>
2021-08-11 19:00:03 +0200

    upiekszone gui

--- apiweather.py
+++ apiweather.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 import json
 import tkinter as tk #gui
@@ -9,7 +10,7 @@ def pokazTemp(miasto):
     #print(wynikZapytania.text)
     ekstrakt = json.loads(wynikZapytania.text)
     #print(ekstrakt['main']['temp']-273.15)
-    return ekstrakt['main']['temp']-273.15
+    return round(ekstrakt['main']['temp']-273.15) 
 
 # while True:
 #     miasto = input("Podaj miasto: ")
@@ -20,19 +21,22 @@ class aplikacja (tk.Frame):
         self.przycisk = tk.Button(self)
         self.przycisk['text']='Pokaz temperature'
         self.pack()
-        self.przycisk.grid(row=1, column=1)
+        self.przycisk.grid(row=2, column=1)
         self.przycisk['command'] = self.pokazTemp
 
-        self.label = tk.Label(self)
-        self.label.grid(row=2, column=1)
+        self.labelOdp = tk.Label(self)
+        self.labelOdp.grid(row=2, column=2)
         self.tuwpisz = tk.Entry(self)
-        self.tuwpisz.grid(row=3, column=1)
+        self.tuwpisz.grid(row=1, column=2)
+        self.labelOpis = tk.Label(self)
+        self.labelOpis.grid(row=1, column=1)
+        self.labelOpis['text']='Wpisz miasto'
     
     
     def pokazTemp(self):
         
         miasto = self.tuwpisz.get()
-        self.label['text']=pokazTemp(miasto)
+        self.labelOdp['text']= str(pokazTemp(miasto)) + " °C"
 
 
 
